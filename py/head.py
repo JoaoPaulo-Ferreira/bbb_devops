52d44d6707d67c61ac48f39424a30d763d19fc89
JoaoPaulo-Ferreira <joaopaulof.soares@gmail.com>
2020-03-31 17:13:19 -0300

    up

--- py/head.py
+++ py/head.py
@@ -15,6 +15,8 @@ duty_span = duty_max - duty_min
 
 PWM.start(SERVO, (100-duty_min), 60.0, 1)
 
+us1 = ultrasound(ECHO_I, TRIGGER_I)
+
 # GPIO.setup(TRIGGER_I, GPIO.OUT)
 # GPIO.setup(TRIGGER_II, GPIO.OUT)
 # GPIO.setup(ECHO_I, GPIO.IN)
