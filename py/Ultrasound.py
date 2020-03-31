8c601b7a8093bcbefcf8c55a84b824b8ab009a77
JoaoPaulo-Ferreira <joaopaulof.soares@gmail.com>
2020-03-31 17:09:07 -0300

    up

---
+++ py/Ultrasound.py
@@ -0,0 +1,39 @@
+import Adafruit_BBIO.GPIO as GPIO
+class ultrasound:
+  def __init__(self, TRIGGER, ECHO):
+    self.TRIGGER = TRIGGER
+    self.ECHO = ECHO
+    GPIO.setup(TRIGGER, GPIO.OUT)
+    GPIO.setup(ECHO, GPIO.IN)
+
+    def distance(self):
+        GPIO.output(self.TRIGGER, GPIO.LOW)
+        time.sleep(0.000005)
+
+        GPIO.output(self.TRIGGER, GPIO.HIGH)
+        time.sleep(0.00002)
+
+        GPIO.output(self.TRIGGER, GPIO.LOW)
+
+        #variaveis para salvar os tempos
+        StartTime = time.time()
+        StopTime = time.time()
+        #variaveis para controle de timeout,
+        #caso fique preso no loop por tempo suficiente para percorrer 2m sai do loop e retorna -1
+        ControlStart = time.time()
+        ControlStop = time.time()
+
+        while GPIO.input(self.ECHO) == 0:
+            StartTime = time.time()
+            ControlStop = time.time()
+            if((((ControlStop - ControlStart) * 34300)/2) >= 200):
+                return -1
+
+        while GPIO.input(self.ECHO) == 1:
+            #print("deb2")
+            StopTime = time.time()
+
+        TimeElapsed = StopTime - StartTime
+        #distance = ((TimeElapsed * 34300) / 2)
+        distance = ((TimeElapsed * 34584) / 2)
+        return distance
---
+++ py/head.py
@@ -0,0 +1,31 @@
+import time
+import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.PWM as PWM
+import Ultrasound
+SERVO = "P8_13"
+TRIGGER_I = "P9_12"
+TRIGGER_II = "P9_16"
+ECHO_I = "P9_23"
+ECHO_II = "P9_14"
+
+
+duty_min = 3
+duty_max = 14.5
+duty_span = duty_max - duty_min
+
+PWM.start(SERVO, (100-duty_min), 60.0, 1)
+
+# GPIO.setup(TRIGGER_I, GPIO.OUT)
+# GPIO.setup(TRIGGER_II, GPIO.OUT)
+# GPIO.setup(ECHO_I, GPIO.IN)
+# GPIO.setup(ECHO_II, GPIO.IN)
+#
+# while True:
+#     angle = input("Angle (0 to 180 x to exit):")
+#     if angle == 'x':
+#         PWM.stop(SERVO)
+#         PWM.cleanup()
+#         break
+#     angle_f = float(angle)
+#     duty = 100 - ((angle_f / 180) * duty_span + duty_min)
+#     PWM.set_duty_cycle(SERVO, duty)
