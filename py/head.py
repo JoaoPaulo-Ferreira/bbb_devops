c649b0e786b913332131a147292f93a57ad3433a
JoaoPaulo-Ferreira <joaopaulof.soares@gmail.com>
2020-04-06 10:01:30 -0300

    up

--- py/head.py
+++ py/head.py
@@ -9,42 +9,31 @@ TRIGGER_II = "P9_16"
 ECHO_I = "P9_23"
 ECHO_II = "P9_14"
 
-#
-# duty_min = 3
-# duty_max = 14.5
-# duty_span = duty_max - duty_min
-
-# PWM.start(SERVO, (100-duty_min), 60.0, 1)
-
 us1 = ultrasound(TRIGGER_I, ECHO_I)
 us2 = ultrasound(TRIGGER_II, ECHO_II)
 motor = servo(SERVO)
 
-cont = 0
-aux = 0
+angle = 0
+INCREASE_MODE = True
 while True:
-    motor.set_angle(cont)
-    dist = us1.distance()
-    time.sleep(0.1)
-    distII = us2.distance()
-    time.sleep(0.1)
-    print("distances = ", dist, " | ", distII, "     angle = ", cont)
-    cont = (cont + 1) % 180
+    while(INCREASE_MODE = True)
+        dist = us1.distance()
+        # time.sleep(0.1)
+        distII = us2.distance()
+        time.sleep(0.1)
+        print("distances = ", dist, " | ", distII, "     angle = ", cont)
+        if(angle == 179):
+            INCREASE_MODE = False
+            break
+        angle = angle + 1
 
-
-    # print ("    Measured Distance = %.1f cm" % distII)
-    # print ("medida ultrasounII = %.2f" % distance)
-# GPIO.setup(TRIGGER_I, GPIO.OUT)
-# GPIO.setup(TRIGGER_II, GPIO.OUT)
-# GPIO.setup(ECHO_I, GPIO.IN)
-# GPIO.setup(ECHO_II, GPIO.IN)
-#
-# while True:
-#     angle = input("Angle (0 to 180 x to exit):")
-#     if angle == 'x':
-#         PWM.stop(SERVO)
-#         PWM.cleanup()
-#         break
-#     angle_f = float(angle)
-#     duty = 100 - ((angle_f / 180) * duty_span + duty_min)
-#     PWM.set_duty_cycle(SERVO, duty)
+    while(INCREASE_MODE = False)
+        dist = us1.distance()
+        # time.sleep(0.1)
+        distII = us2.distance()
+        time.sleep(0.1)
+        print("distances = ", dist, " | ", distII, "     angle = ", cont)
+        if(angle == 0):
+            INCREASE_MODE = True
+            break
+        angle = angle - 1
